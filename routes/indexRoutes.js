fb61b018bc011231589ae592031d2fea6acc0ec3
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-12-05 17:27:56 +0100

    handle routes to the homepage

---
+++ routes/indexRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import IndexRouter from '../controllers/indexController.js'
+
+const router = Router();
+
+router.get('/', IndexRouter.homepage);
+router.get('/about', IndexRouter.about);
+
+export default router;
