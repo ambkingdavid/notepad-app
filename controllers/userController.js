f45de3adcbd74ed00adc12e157ba9b53ebe1e24b
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-12-08 11:28:10 +0000

    change file struct

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,16 @@
 import User from '../models/user.model.js';
+import Note from '../models/note.model.js';
 
 
 class UserController {
   static async dashboard(req, res, next) {
-    res.status(200).render('dashboard', {
+    res.status(200).render('dashboard/index', {
       layout: '../views/layouts/dashboardPage',
-    })
+    });
+  }
+
+  static async addNote(req, res, next) {
+    res.status(201).redirect('/dashboard');
   }
 }
 
