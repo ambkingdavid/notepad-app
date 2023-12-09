88d86270ceb3b499bda7bd63335dc584b33d9ace
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-12-09 11:33:09 -0800

    add routes to get notes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ import isLoggedIn from '../middleware/authentication.js';
 const router = Router();
 
 router.get('/dashboard', isLoggedIn,  UserController.dashboard);
+router.get('/dashboard/note/:id', isLoggedIn, UserController.getNote);
 
 export default router;
