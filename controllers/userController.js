c278c385311f7e58fd60e9963a6a5bf31e8ef974
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-12-14 08:50:02 -0800

    add controllers to handle user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ import Note from '../models/note.model.js';
 
 class UserController {
   static async dashboard(req, res, next) {
+    const user = await User.findById(req.user.id);
     const query = {
       createdBy: req.user.id,
     };
@@ -21,7 +22,6 @@ class UserController {
     notesObj = notesObj.skip(skip).limit(limit);
 
     const numOfPage = Math.ceil(totalNotes / limit);
-    console.log(numOfPage)
 
     const notesList = await notesObj;
 
@@ -30,6 +30,7 @@ class UserController {
 
     res.status(200).render('dashboard/index', {
       layout: '../views/layouts/dashboardPage',
+      user,
       notes,
       totalNotes,
       numOfPage,
@@ -38,8 +39,55 @@ class UserController {
     });
   }
 
-  static async addNote(req, res, next) {
-    res.redirect('/dashboard');
+  static async addNote(req, res) {
+    const { title, content } = req.body;
+    req.body.createdBy = req.user.id;
+    if (!title) {
+      const message = 'Title field required'
+      return res.status(400).redirect('/dashboard/new-note')
+    }
+    const newNote = await Note.create(req.body);
+
+    res.status(201).redirect(`/dashboard/note/${newNote._id}`);
+  }
+
+  static async updateNote(req, res) {
+    const { title, content } = req.body;
+    if (!title || !content) {
+      return res.status(400).redirect(`/dashboard/note/${req.params.id}`);
+    }
+
+    try {
+      await Note.findByIdAndUpdate(req.params.id, req.body).where(req.user.id);
+      res.status(500).redirect(`/dashboard/note/${req.params.id}`);
+    } catch (err) {
+      console.log(err)
+      res.status(500).redirect(`/dashboard/note/${req.params.id}`);
+    }
+
+  }
+
+  static async deleteNote(req, res) {
+    const note = await Note.findOne({ _id: req.params.id}).where(req.user.id);
+    if (!note) {
+      return res.status(404).render('404');
+    }
+    await Note.findByIdAndDelete(req.params.id).where(req.user.id);
+    res.status(200).redirect('/dashboard');
+  }
+
+  static async newNotePage(req, res) {
+    const user = await User.findById(req.user.id);
+    const locals = {
+      title: 'NotePad',
+      description: 'Create Note',
+    }
+
+    res.render('dashboard/newNote', {
+      locals,
+      layout: '../views/layouts/dashboardPage',
+      user,
+    });
   }
 
   static async getNote(req, res, next) {
@@ -49,6 +97,8 @@ class UserController {
     }).where({
       createdBy: req.user.id,
     }).lean()
+    const formatDate = new Date(note.createdAt);
+    note.createdAt = formatDate.toLocaleString();
     res.render('dashboard/viewNote', {
       layout: '../views/layouts/dashboardPage',
       note,
