036a6255704a7f4968e8308f378b25f6051bb0a8
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-12-14 08:51:34 -0800

    add routed to access user data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,18 @@ import isLoggedIn from '../middleware/authentication.js';
 
 const router = Router();
 
+// GET
 router.get('/dashboard', isLoggedIn,  UserController.dashboard);
 router.get('/dashboard/note/:id', isLoggedIn, UserController.getNote);
+router.get('/dashboard/new-note', isLoggedIn, UserController.newNotePage);
+
+// DELETE
+router.delete('/dashboard/note/:id', isLoggedIn, UserController.deleteNote);
+
+// PUT
+router.put('/dashboard/note/:id', isLoggedIn, UserController.updateNote);
+
+// POST
+router.post('/dashboard/note/add-note', isLoggedIn, UserController.addNote);
 
 export default router;
